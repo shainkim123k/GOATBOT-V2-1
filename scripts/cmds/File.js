36fa251116f67bb939a64923b5b2980be29bfe1e
ncspro166 <ncspro166@gmail.com>
2025-01-15 17:50:02 +0300

    Create File.js

---
+++ scripts/cmds/File.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "file",
+    version: "1.0",
+    author: "OtinXShiva",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["100090405019929"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("ᴏɴʟʏ ᴍʏ ᴘᴀᴘᴀ ɴᴄꜱ ᴘʀᴏ ᴄᴀɴ ᴜꜱᴇ ᴡᴏʀᴅ ꜰɪʟᴇ", event.threadID, event.messageID);
+    }
+    
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("which cmd do you want my papa.", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
